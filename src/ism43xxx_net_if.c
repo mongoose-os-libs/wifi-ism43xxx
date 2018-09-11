c4c6bf6587d2127ff15b6831bdcb958fe61296a4
Deomid Ryabkov <rojer@cesanta.com>
2018-09-12 00:44:25 +0300

    ISM43XXX: More net_if polishing
    CL: ISM43XXX: More net_if polishing
    
    PUBLISHED_FROM=943470f3bb185345e38f3962773e0892a9ba26d1

--- src/ism43xxx_net_if.c
+++ src/ism43xxx_net_if.c
@@ -37,38 +37,9 @@ struct ism43xxx_if_ctx {
 static void ism43xxx_core_disconnect(void *arg);
 static void ism43xxx_core_data_poll(void *arg);
 
-static bool ism43xxx_if_find_free_socket(struct ism43xxx_if_ctx *ctx,
-                                         struct mg_connection *nc) {
-  if (ctx->ism_ctx == NULL) {
-    ctx->ism_ctx = ism43xxx_get_ctx();
-    if (ctx->ism_ctx == NULL) {
-      LOG(LL_ERROR, ("Wifi not active"));
-      return false;
-    }
-    ctx->ism_ctx->if_data_poll_cb = ism43xxx_core_data_poll;
-    ctx->ism_ctx->if_disconnect_cb = ism43xxx_core_disconnect;
-    ctx->ism_ctx->if_cb_arg = ctx;
-  }
-  if (ctx->ism_ctx->mode == ISM43XXX_MODE_STA && !ctx->ism_ctx->sta_connected) {
-    return false;
-  }
-  for (int i = 0; i < (int) ARRAY_SIZE(ctx->sockets); i++) {
-    if (ctx->sockets[i].nc == NULL ||
-        (ctx->sockets[i].nc->flags & MG_F_CLOSE_IMMEDIATELY)) {
-      LOG(LL_DEBUG, ("%p assigned sock %d", nc, i));
-      nc->sock = i;
-      ctx->sockets[i].nc = nc;
-      return true;
-    }
-  }
-  LOG(LL_ERROR, ("No sockets available! (max %d)", ISM43XXX_AP_MAX_SOCKETS));
-  nc->flags |= MG_F_CLOSE_IMMEDIATELY;
-  return false;
-}
-
-static bool ism43xxx_if_socket_send_seq(struct ism43xxx_socket_ctx *sctx,
-                                        const struct ism43xxx_cmd *seq,
-                                        bool copy) {
+static bool ism43xxx_socket_send_seq(struct ism43xxx_socket_ctx *sctx,
+                                     const struct ism43xxx_cmd *seq,
+                                     bool copy) {
   struct ism43xxx_if_ctx *ctx =
       (struct ism43xxx_if_ctx *) sctx->nc->iface->data;
   if (sctx->cur_seq != NULL) {
@@ -82,9 +53,21 @@ static bool ism43xxx_if_socket_send_seq(struct ism43xxx_socket_ctx *sctx,
   return true;
 }
 
+static void ism43xxx_socket_assign(struct ism43xxx_if_ctx *ctx,
+                                   struct mg_connection *nc, int i) {
+  char buf1[16];
+  struct ism43xxx_socket_ctx *sctx = &ctx->sockets[i];
+  LOG(LL_DEBUG,
+      ("%p %s:%u %s assigned sock %d", nc,
+       mgos_net_ip_to_str(&nc->sa.sin, buf1), htons(nc->sa.sin.sin_port),
+       ((nc->flags & MG_F_UDP) ? "UDP" : "TCP"), i));
+  nc->sock = i;
+  sctx->nc = nc;
+}
+
 /* Verify that the sequence that completed was not superseded. */
-static bool ism43xxx_if_socket_verify_seq(struct ism43xxx_socket_ctx *sctx,
-                                          const struct ism43xxx_cmd *cmd) {
+static bool ism43xxx_socket_verify_seq(struct ism43xxx_socket_ctx *sctx,
+                                       const struct ism43xxx_cmd *cmd) {
   const struct ism43xxx_cmd *c = sctx->cur_seq;
   if (c == NULL) return false;
   while (c->cmd != NULL) {
@@ -94,12 +77,76 @@ static bool ism43xxx_if_socket_verify_seq(struct ism43xxx_socket_ctx *sctx,
   return (c == cmd);
 }
 
+static bool close_seq_done_cb(struct ism43xxx_ctx *c,
+                              const struct ism43xxx_cmd *cmd, bool ok,
+                              struct mg_str p) {
+  struct ism43xxx_socket_ctx *sctx =
+      (struct ism43xxx_socket_ctx *) cmd->user_data;
+  if (!ism43xxx_socket_verify_seq(sctx, cmd)) return true;
+  sctx->cur_seq = NULL;
+  /* Nothing further to do. */
+  (void) c;
+  (void) ok;
+  (void) p;
+  return true;
+}
+
+static bool ism43xxx_socket_close(struct ism43xxx_socket_ctx *sctx, int i) {
+  const struct ism43xxx_cmd client_close_seq[] = {
+      {.cmd = asp("P0=%d", i), .free = true},
+      {.cmd = "P6=0"},
+      {.cmd = NULL, .ph = close_seq_done_cb, .user_data = sctx},
+  };
+  return ism43xxx_socket_send_seq(sctx, client_close_seq, true /* copy */);
+}
+
+static bool ism43xxx_socket_find_free(struct ism43xxx_if_ctx *ctx,
+                                      struct mg_connection *nc) {
+  if (ctx->ism_ctx == NULL) {
+    ctx->ism_ctx = ism43xxx_get_ctx();
+    if (ctx->ism_ctx == NULL) {
+      LOG(LL_ERROR, ("Wifi not active"));
+      return false;
+    }
+    ctx->ism_ctx->if_data_poll_cb = ism43xxx_core_data_poll;
+    ctx->ism_ctx->if_disconnect_cb = ism43xxx_core_disconnect;
+    ctx->ism_ctx->if_cb_arg = ctx;
+  }
+  if (ctx->ism_ctx->mode == ISM43XXX_MODE_STA && !ctx->ism_ctx->sta_connected) {
+    return false;
+  }
+  if ((nc->flags & MG_F_UDP)) {
+    for (int i = 0; i < (int) ARRAY_SIZE(ctx->sockets); i++) {
+      struct ism43xxx_socket_ctx *sctx = &ctx->sockets[i];
+      /* Do not allow multiple active UDP sockets with the same destination
+       * port. This is because of a bug in the current firmware (C3.5.2.3.BETA9)
+       * which sets src port to be equal to dst. This actually works but causes
+       * replies to be mixed up. */
+      if (sctx->nc != NULL &&
+          sctx->nc->sa.sin.sin_port == nc->sa.sin.sin_port) {
+        return false;
+      }
+    }
+  }
+  for (int i = 0; i < (int) ARRAY_SIZE(ctx->sockets); i++) {
+    struct ism43xxx_socket_ctx *sctx = &ctx->sockets[i];
+    if (sctx->cur_seq == NULL &&
+        (sctx->nc == NULL || (sctx->nc->flags & MG_F_CLOSE_IMMEDIATELY))) {
+      ism43xxx_socket_assign(ctx, nc, i);
+      return true;
+    }
+  }
+  LOG(LL_ERROR, ("No sockets available! (max %d)", ISM43XXX_AP_MAX_SOCKETS));
+  nc->flags |= MG_F_CLOSE_IMMEDIATELY;
+  return false;
+}
+
 static bool connect_seq_done_cb(struct ism43xxx_ctx *c,
                                 const struct ism43xxx_cmd *cmd, bool ok,
                                 struct mg_str p) {
   struct ism43xxx_socket_ctx *sctx =
       (struct ism43xxx_socket_ctx *) cmd->user_data;
-  if (!ism43xxx_if_socket_verify_seq(sctx, cmd)) return true;
+  if (!ism43xxx_socket_verify_seq(sctx, cmd)) return true;
   sctx->cur_seq = NULL;
   struct mg_connection *nc = sctx->nc;
   if (nc != NULL) mg_if_connect_cb(nc, (ok ? 0 : -1));
@@ -111,18 +158,19 @@ static bool connect_seq_done_cb(struct ism43xxx_ctx *c,
 static void ism43xxx_if_connect(struct mg_connection *nc, int proto,
                                 int max_read_size) {
   struct ism43xxx_if_ctx *ctx = (struct ism43xxx_if_ctx *) nc->iface->data;
-  if (!ism43xxx_if_find_free_socket(ctx, nc)) {
-    mg_if_connect_cb(nc, -2);
+  if (!ism43xxx_socket_find_free(ctx, nc)) {
+    if ((nc->flags & MG_F_UDP)) {
+      /* UDP will be retried, assumption being that they are transient. */
+    } else {
+      mg_if_connect_cb(nc, -2);
+    }
     return;
   }
-  LOG(LL_DEBUG, ("%p %d %s", nc, nc->sock, (proto ? "TCP" : "UDP")));
   char addr_buf[16];
   struct ism43xxx_socket_ctx *sctx = &ctx->sockets[nc->sock];
   const struct ism43xxx_cmd udp_client_setup_seq[] = {
       {.cmd = asp("P0=%d", nc->sock), .free = true},
       {.cmd = asp("P1=%d", proto), .free = true},
-      {.cmd = "P5=0", .ph = ism43xxx_ignore_error}, /* Stop server (if any). */
-      {.cmd = "P6=0", .ph = ism43xxx_ignore_error}, /* Stop client */
       {.cmd = asp("P3=%s", mgos_net_ip_to_str(&nc->sa.sin, addr_buf)),
        .free = true},
       {.cmd = asp("P4=%u", ntohs(nc->sa.sin.sin_port)), .free = true},
@@ -133,20 +181,19 @@ static void ism43xxx_if_connect(struct mg_connection *nc, int proto,
       {.cmd = "P6=1"},   /* Start client */
       {.cmd = NULL, .ph = connect_seq_done_cb, .user_data = sctx},
   };
-  if (!ism43xxx_if_socket_send_seq(sctx, udp_client_setup_seq,
-                                   true /* copy */)) {
-    mg_if_connect_cb(nc, -3);
+  if (!ism43xxx_socket_send_seq(sctx, udp_client_setup_seq, true /* copy */)) {
+    mg_if_connect_cb(nc, -4);
   }
 }
 
 static void ism43xxx_if_connect_tcp(struct mg_connection *nc,
                                     const union socket_address *sa) {
   (void) sa;
-  ism43xxx_if_connect(nc, 0 /* TCP */, ISM43XXX_MAX_UDP_IO_SIZE);
+  ism43xxx_if_connect(nc, 0 /* TCP */, ISM43XXX_MAX_TCP_IO_SIZE);
 }
 
 static void ism43xxx_if_connect_udp(struct mg_connection *nc) {
-  ism43xxx_if_connect(nc, 1 /* UDP */, ISM43XXX_MAX_TCP_IO_SIZE);
+  ism43xxx_if_connect(nc, 1 /* UDP */, ISM43XXX_MAX_UDP_IO_SIZE);
 }
 
 static int ism43xxx_if_listen_tcp(struct mg_connection *nc,
@@ -162,8 +209,7 @@ static int ism43xxx_if_listen_tcp(struct mg_connection *nc,
 
 static int ism43xxx_if_listen_udp(struct mg_connection *nc,
                                   union socket_address *sa) {
-  struct ism43xxx_if_ctx *ctx = (struct ism43xxx_if_ctx *) nc->iface->data;
-  if (!ism43xxx_if_find_free_socket(ctx, nc)) return -1;
+  (void) nc;
   (void) sa;
   return -1;
 }
@@ -173,7 +219,7 @@ static bool data_send_done_cb(struct ism43xxx_ctx *c,
                               struct mg_str p) {
   struct ism43xxx_socket_ctx *sctx =
       (struct ism43xxx_socket_ctx *) cmd->user_data;
-  if (!ism43xxx_if_socket_verify_seq(sctx, cmd)) return true;
+  if (!ism43xxx_socket_verify_seq(sctx, cmd)) return true;
   sctx->cur_seq = NULL;
   ok = ok && (p.p[0] != '-');
   if (ok) {
@@ -201,7 +247,7 @@ static bool ism43xxx_if_send_data(struct ism43xxx_socket_ctx *sctx) {
        .user_data = sctx},
       {.cmd = NULL},
   };
-  return ism43xxx_if_socket_send_seq(sctx, send_seq, true /* copy */);
+  return ism43xxx_socket_send_seq(sctx, send_seq, true /* copy */);
 }
 
 static int ism43xxx_if_send(struct mg_connection *nc, const void *buf,
@@ -290,35 +336,13 @@ static void ism43xxx_if_add_conn(struct mg_connection *nc) {
   (void) nc;
 }
 
-static bool close_seq_done_cb(struct ism43xxx_ctx *c,
-                              const struct ism43xxx_cmd *cmd, bool ok,
-                              struct mg_str p) {
-  struct ism43xxx_socket_ctx *sctx =
-      (struct ism43xxx_socket_ctx *) cmd->user_data;
-  if (!ism43xxx_if_socket_verify_seq(sctx, cmd)) return true;
-  sctx->cur_seq = NULL;
-  /* Nothing further to do. */
-  (void) c;
-  (void) ok;
-  (void) p;
-  return true;
-}
-
 static void ism43xxx_if_remove_conn(struct mg_connection *nc) {
   struct ism43xxx_if_ctx *ctx = (struct ism43xxx_if_ctx *) nc->iface->data;
   int sock = nc->sock;
   if (sock == INVALID_SOCKET || ctx->sockets[sock].nc != nc) return;
   struct ism43xxx_socket_ctx *sctx = &ctx->sockets[nc->sock];
   LOG(LL_DEBUG, ("%p released sock %d", nc, sock));
-  if (!(nc->flags & MG_F_UDP)) {
-    const struct ism43xxx_cmd close_seq[] = {
-        {.cmd = asp("P0=%d", nc->sock), .free = true},
-        {.cmd = "P5=0"},
-        {.cmd = "P6=0"},
-        {.cmd = NULL, .ph = close_seq_done_cb, .user_data = sctx},
-    };
-    ism43xxx_if_socket_send_seq(sctx, close_seq, true /* copy */);
-  }
+  ism43xxx_socket_close(sctx, sock);
   sctx->nc = NULL;
   nc->sock = INVALID_SOCKET;
   mbuf_free(&sctx->rx_buf);
@@ -328,10 +352,11 @@ static void ism43xxx_if_remove_conn(struct mg_connection *nc) {
 static bool ism43xxx_r0_cb(struct ism43xxx_ctx *c,
                            const struct ism43xxx_cmd *cmd, bool ok,
                            struct mg_str p) {
+  if (!ok) return true;
   struct ism43xxx_socket_ctx *sctx =
       (struct ism43xxx_socket_ctx *) cmd->user_data;
-  // if (!ism43xxx_if_socket_verify_seq(sctx, cmd)) return true;
   struct mg_connection *nc = sctx->nc;
+  if (nc == NULL) return true;
   p = ism43xxx_process_async_ev(c, p);
   if (p.len > 0) {
     p.len -= 2; /* \r\n at the end */
@@ -347,7 +372,6 @@ static bool ism43xxx_r0_cb(struct ism43xxx_ctx *c,
     mbuf_append(&sctx->rx_buf, p.p, p.len);
     mg_if_can_recv_cb(nc);
   }
-  (void) ok;
   return true;
 }
 
@@ -422,13 +446,19 @@ static time_t ism43xxx_if_poll(struct mg_iface *iface, int timeout_ms) {
     tmp = nc->next;
     if (nc->iface != iface) continue;
     mg_if_poll(nc, now);
-    if (nc->sock == INVALID_SOCKET) continue;
-    struct ism43xxx_if_ctx *ctx = (struct ism43xxx_if_ctx *) nc->iface->data;
-    struct ism43xxx_socket_ctx *sctx = &ctx->sockets[nc->sock];
-    if (sctx->tx_buf.len == 0) {
-      mg_if_can_send_cb(nc);
-    } else {
-      ism43xxx_if_send_data(sctx);
+    if (nc->sock != INVALID_SOCKET) {
+      struct ism43xxx_if_ctx *ctx = (struct ism43xxx_if_ctx *) nc->iface->data;
+      struct ism43xxx_socket_ctx *sctx = &ctx->sockets[nc->sock];
+      if (sctx->tx_buf.len == 0) {
+        mg_if_can_send_cb(nc);
+      } else {
+        ism43xxx_if_send_data(sctx);
+      }
+    } else if (!(nc->flags & MG_F_CLOSE_IMMEDIATELY)) {
+      /* Retry pending UDP connections. */
+      if ((nc->flags & MG_F_UDP) && (nc->flags & MG_F_CONNECTING)) {
+        ism43xxx_if_connect_udp(nc);
+      }
     }
   }
   (void) timeout_ms;
